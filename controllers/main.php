9f2dbe8dc9babb7a32fcf38790f368f98264c025
Jeremy Keeshin <jkeesh@gmail.com>
2010-12-18 03:45:05 -0600

    remove get,post test

--- controllers/main.php
+++ controllers/main.php
@@ -2,8 +2,6 @@
 class IndexHandler extends ToroHandler {
   
     public function get() {
-    	print_r($_GET);
-	print_r($_POST);
         // for now this is hard coded .. need to setup DB access
         $studentdir = "2_ADTS";
         //$dirname="/afs/ir.stanford.edu/class/".$classname."/submissions/".$sl."/";
