4aea6e0c6e826f897b9aaf50cbf01cd6623509c7
Eric Conner <econner@stanford.edu>
2010-12-18 14:07:02 -0700

    Added static factory methods to assignment comment model.

--- models/AssignmentComment.php
+++ models/AssignmentComment.php
@@ -9,27 +9,75 @@ class AssignmentComment extends Model {
   private $LineNumber;
   private $CommentText;
   
-  public function __construct($ID, $AssignmentFile, $LineNumber, $CommentText) {
+  public function __construct() {
     parent::__construct();
-    $this->ID = $ID;
-    $this->AssignmentFile = $AssignmentFile;
-    $this->LineNumber = $LineNumber;
-    $this->CommentText = $CommentText;
   }
   
+  /*
+  * Saves the current assignment comment's state to 
+  * the database.
+  */
   public function save() {
     $query = "REPLACE INTO " . ASSIGNMENT_COMMENT_TABLE . 
-              " VALUES(" . $this->ID . ", " . 
-              $this->AssignmentFile . ", " . 
-              $this->LineNumber . ", '" . 
-              $this->CommentText . "');";
-    $this->db->exec($query);
+              " VALUES(" . mysql_real_escape_string($this->ID) . ", " . 
+              mysql_real_escape_string($this->AssignmentFile) . ", " . 
+              mysql_real_escape_string($this->LineNumber) . ", '" . 
+              mysql_real_escape_string($this->CommentText) . "');";
+    $this->conn->exec($query);
   }
   
+  public static function create($AssignmentFile, $LineNumber, $CommentText) {
+    $instance = new self();
+    $instance->fill(array(0, $AssignmentFile, $LineNumber, $CommentText));
+    return $instance;
+  }
+  
+  /*
+  * Load from an id
+  */
+  public static function load($ID) {
+    $query = "SELECT * FROM " . ASSIGNMENT_COMMENT_TABLE .
+              " WHERE ID = " . mysql_real_escape_string($ID) . ";";
+    $instance = new self();
+    $sth = $instance->conn->query($query);
+
+    $sth->setFetchMode(PDO::FETCH_NUM);
+    if($row = $sth->fetch()) {
+      $instance->fill($row);
+      return $instance;
+    }
+    return null;
+  }
+  
+  /*
+  * Getters and Setters
+  */
+  
+  public function fill(array $row) { 
+    $this->setID($row[0]);
+    $this->setAssignmentFile($row[1]);
+    $this->setLineNumber($row[2]);
+    $this->setCommentText($row[3]);
+  }
+  
+  public function setID($ID) { $this->ID = $ID; }
+  public function getID() { return $this->ID; }
+  
+  public function setAssignmentFile($AssignmentFile) { $this->AssignmentFile = $AssignmentFile; }
+  public function getAssignmentFile() { return $AssignmentFile; }
+  
+  public function setLineNumber($LineNumber) { $this->LineNumber = $LineNumber; }
+  public function getLineNumber() { return $this->LineNumber; }
+  
+  public function setCommentText($CommentText) { $this->CommentText = $CommentText; }
+  public function getCommentText() { return $CommentText; }
 }
 
-$ac = new AssignmentComment(0, 1, 23, "test test");
+$ac = AssignmentComment::create(12, 23, "test test");
 $ac->save();
-
+print_r($ac);
+echo "<br><br>";
+$ac = AssignmentComment::load(1);
+print_r($ac);
 
 ?>
--- models/Model.php
+++ models/Model.php
@@ -1,14 +1,40 @@
 <?php
 require_once("../config.php");
+
+/*
+* Singleton factory for producing a db connection
+* Rationale behind this / credit here:
+* http://stackoverflow.com/questions/130878/global-or-singleton-for-database-connection
+*/
+class ConnectionFactory
+{
+  private static $factory;
+  public static function getFactory()
+  {
+    if (!self::$factory)
+      self::$factory = new ConnectionFactory();
+    return self::$factory;
+  }
+
+  private $db;
+  public function getConnection() {
+    if (!$this->db) {
+      try {
+        $this->db = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DATABASE, MYSQL_USERNAME, MYSQL_PASSWORD);
+      } catch(PDOException $e) {
+        echo $e->getMessage();
+        return null;
+      }
+    }
+    return $this->db;
+  }
+}
+
+
 class Model {
-  protected $db;
+  public $conn;
   
   public function __construct() {
-    try {
-      $this->db = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DATABASE, MYSQL_USERNAME, MYSQL_PASSWORD);
-      #$this->db = new PDO("sqlite:paperless.db");
-    } catch(PDOException $e) {
-      echo $e->getMessage();
-    }
+    $this->conn = ConnectionFactory::getFactory()->getConnection();
   }
 }
