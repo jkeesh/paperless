7c2948a414fc3ff452b441bafe1e74c7937bd610
Jeremy Keeshin <jkeesh@gmail.com>
2010-12-20 01:47:34 -0600

    modify local config

--- config_local.php
+++ config_local.php
@@ -1,6 +1,5 @@
 <?php
-//This is a configuration file that is not tracked by git. The difference is that
-//there is a different configuration file locally and on the web.
+//This is a configuration file for running paperless locally
 
 define('BASE_DIR', dirname(__FILE__));
 define('DUMMYDIR', '2_ADTS');
@@ -8,5 +7,11 @@ define('USERNAME', 'zhoud');
 define('CLASSNAME', 'cs106x');
 define('SUBMISSIONS_DIR', 'submissions');
 define('ROOT_URL', 'http://localhost:8888/paperless/'); 
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USERNAME', 'root');
+define('MYSQL_PASSWORD', 'root');
+define('MYSQL_DATABASE', 'paperless');
+define('ASSIGNMENT_COMMENT_TABLE', "AssignmentComments");
+define('ASSIGNMENT_FILE_TABLE', 'AssignmentFiles');
 
 ?>
