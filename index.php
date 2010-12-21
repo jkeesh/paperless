3805f7d2679378ccda74e3aceda832d1ee0d46a0
Jeremy Keeshin <jkeesh@gmail.com>
2010-12-20 21:48:07 -0600

    comment out both check, this can be handled later

--- index.php
+++ index.php
@@ -34,10 +34,10 @@ class ToroHandler {
 	  $this->smarty->assign("is_section_leader", $is_sl);
 	  $this->smarty->assign("is_student", $is_student);
 	
-	  if($is_sl && $is_student){
-		 echo "sl and student";
-		 return;
- 	  }
+	  // if($is_sl && $is_student){
+	  // 		 echo "sl and student";
+	  // 		 return;
+	  //  	  }
 	  if($is_sl)
 	  	$this->smarty->assign("students", Model::getStudentsForSectionLeader(USERNAME));
   }
