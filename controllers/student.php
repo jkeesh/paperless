76588582bb93440f239c47546fb257cebaf0cb0a
Jeremy Keeshin <jkeesh@gmail.com>
2010-12-20 19:51:23 -0600

    students associated with their submissions, if they had only one

--- controllers/student.php
+++ controllers/student.php
@@ -5,12 +5,31 @@ class StudentHandler extends ToroHandler {
     public function get($student) {
         // for now hard coded .. need to setup DB access
         $dirname = SUBMISSIONS_DIR . "/" . USERNAME ."/";
+		
         $assns = $this->getDirEntries($dirname);
-        
+
+		//information will be an associative array where index i holds
+		//the assignment and student directory information as keys
+		$information = array();
+		
+		$i = 0;
+        //for every assignment, go find ones that belong to the student
+		foreach($assns as $assn){
+		 	$dir = SUBMISSIONS_DIR ."/". USERNAME ."/" . $assn ."/";
+			$student_submissions = $this->getDirEntries($dir);
+			$information[$i]['assignment'] = $assn;
+			foreach($student_submissions as $submission){
+				if(strpos($submission, $student) !== false){
+					$information[$i]['studentdir'] = $submission;
+				}
+			}
+			$i++;
+		}
+
+		//print_r($information);
+
         // assign template vars
-        $this->smarty->assign("student", htmlentities($student));
-        $this->smarty->assign("assignments", $assns);
-        
+ 	  	$this->smarty->assign("information", $information);  
         // display the template
         $this->smarty->display("student.html");
     }
