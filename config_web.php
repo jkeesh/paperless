36a1ff647403559645dc1a644c398f7f6da2456a
Jeremy Keeshin <jkeesh@gmail.com>
2010-12-22 01:21:24 -0600

    config web modified

--- config_web.php
+++ config_web.php
@@ -7,6 +7,14 @@
 	//$sunetid = $_ENV['WEBAUTH_USER'];
 	//$username = strtolower($username);
 	
+	define('POSITION_STUDENT', 1);
+	define('POSITION_APPLICANT', 2);
+	define('POSITION_COURSE_HELPER', 3);
+	define('POSITION_SECTION_LEADER', 4);
+	define('POSITION_TEACHING_ASSISTANT', 5);
+	define('POSITION_LECTURER', 6);
+	define('POSITION_COORDINATOR', 7);
+	
 	$username = "jkeeshin";
 	$classname = "cs106a";
 	$dummy = "karel";
