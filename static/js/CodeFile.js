cf7f76111835be1bd3b8817bbb54d5a052286730
Eric Conner <econner@stanford.edu>
2010-12-22 14:40:12 -0700

    Mostly modularized the javascript.. for now it is in separate files to make it easier to deal with.  Still lots of dependencies but its a start at cutting them down.

---
+++ static/js/CodeFile.js
@@ -0,0 +1,168 @@
+function /* class */ CodeFile(filename, prefix) {
+   if(!shortcuts_added)
+      addShortcuts();
+   
+   this.comment_list = new Array(); // TODO make this hold a list of Comment objects
+   this.filename = filename;
+   this.prefix = prefix;
+   this.fileID = this.prefix; // right now pass the filename, and file id
+   
+   this.selected_range_start;
+   this.selected_range_end;
+   this.selected_ranges = [];
+   this.last_comment = null;
+   
+   var self = this;
+
+   this.isLineSelected = function(line_no) {
+      for (var i = 0; i < this.selected_ranges.length; i++) {
+         var range = this.selected_ranges[i];
+         if (line_no >= range.lower && line_no <= range.higher) {
+            return true;
+         }
+      }
+      
+      return false;
+   }
+   
+   this.addHandlers = function() {
+      var line_no = 1;
+      do {
+         var line = this.getLine(line_no);
+         line.bind("mousedown", {code_file : this}, CodeFile.mousePressed);
+         line.bind("mouseenter", {code_file : this}, CodeFile.mouseEntered);
+         line_no++;
+      } while(line.size() != 0);
+   }
+   
+   this.getLine = function(line_no) {
+      var id = "#file" + this.fileID;
+      var theclass = '.number' + line_no;
+      return $(theclass, id);
+   }
+   
+   this.getLineNumber = function(line) {
+      var newline = $(line).attr("class");
+      var pattern = /line number(\d+) .*/;
+      var result = pattern.exec(newline)[1];
+      return parseInt(result);
+   }
+   
+   this.hiliteLineNo = function(line_no) {
+      var id = "#file" + this.fileID;
+      var theclass = '.number' + line_no;
+      $(theclass, id).addClass('highlighted');
+   }
+   
+   this.unhiliteLineNo = function(line_no) {
+      var id = "#file" + this.fileID;
+      var theclass = '.number' + line_no;
+      $(theclass, id).removeClass('highlighted');
+   }
+   
+   this.hiliteLine = function (line) {
+      var line_no = this.getLineNumber(line);
+      $(line).addClass("highlighted");
+   }
+   
+   this.unhiliteLine = function (line) {
+      $(line).removeClass("highlighted");
+   }
+   
+   /* Unhighlights the range passed in as a parameter */
+   this.unhighlightRange = function(range){
+      for (var i = range.lower; i <= range.higher; i++) {
+         this.unhiliteLineNo(i);
+      }  
+   }
+   
+   this.getCommentFromID = function(commentID) {
+      for(var i = 0; i < this.comment_list.length; i++) {
+         if(commentID == "c" + this.comment_list[i].range.toString())
+            return this.comment_list[i];
+      }
+      return null;
+   }
+   
+   this.removeCommentFromID = function(commentID) {
+      for(var i = 0; i < this.comment_list.length; i++) {
+         if(commentID == "c" + this.comment_list[i].range.toString())
+            this.comment_list.splice(i, i);
+      }
+   }
+   
+   this.addComment = function(comment) {
+      this.comment_list.push(comment);
+      this.addCommentDiv(comment.text, comment.range);
+   }
+   
+   this.addCommentDiv = function(text, range){
+      var range_text = range.toString();
+      var comment_id = "c" + range_text;
+      var element_id = "e" + range_text;
+      var top_offset = 17.6 * (range.lower) + 200; //200 is codeposition offset in style.css , 17 is line height;
+      var style_position = "position:absolute; top:"+ top_offset +"px; right: 100px;";
+
+
+      $('#comments' + this.fileID).append("<div id='"+ element_id +"'><a href=\"javascript:edit("+ this.fileID + ",'" + comment_id + "')\"> <div id='" + comment_id
+      +"' style='" + style_position +"' class='commentbox'>"
+      // + " File: " + this.filename
+      // + " Line: "+ range_text + ": "
+      + "<span id='ctext" + range_text + "'>" + text + "</span>"
+      + "</div></a></div>");
+   }
+   
+   this.addHandlers();
+}
+
+CodeFile.mousePressed = function(event) {
+   code_file = event.data.code_file;
+   if (dragging_in_file != null && dragging_in_file != code_file) {
+      return false;
+   }
+   
+   var line_no = code_file.getLineNumber(this);
+   if (current_dialog != null || code_file.isLineSelected(line_no)) {
+      return false;
+   }
+   
+   dragging_in_file = code_file;
+   code_file.selected_range_start = code_file.selected_range_end = line_no;
+   code_file.hiliteLine(this);
+   event.data.code_file.dragging = true;
+   return false;
+}
+
+CodeFile.mouseEntered = function(event) {
+   code_file = event.data.code_file;
+   if (dragging_in_file != code_file) {
+      return;
+   }
+   
+   var line_no = code_file.getLineNumber(this);
+   
+   // Trim range so it doesn't overlap any already selected lines.
+   var increment = (line_no >= code_file.selected_range_start ? 1 : -1);
+   for (var i = code_file.selected_range_start; !(code_file.isLineSelected(i)); i += increment) {
+      new_line_no = i;
+      if (i == line_no) {
+         break;
+      }
+   }
+   line_no = new_line_no;
+   
+   old_range = new LineRange(code_file.selected_range_start, code_file.selected_range_end);
+   range = new LineRange(code_file.selected_range_start, line_no);
+   range.each(function(line_no) {
+            code_file.hiliteLineNo(line_no);
+            });
+   
+   for (var i = old_range.lower; i < range.lower; i++) {
+      code_file.unfLineNo(i);
+   }
+   for (var i = old_range.higher; i > range.higher; i--) {
+      code_file.unhiliteLineNo(i);
+   }
+   
+   code_file.selected_range_end = line_no;
+}
--- static/js/feedback.js
+++ static/js/feedback.js
@@ -36,32 +36,38 @@ function SafeFunction(func){
 
 function addShortcuts(){
 	shortcut.add("tab", function(data) {
-				 var code_file = code_files[current_file_id];
-				 if(current_range && current_dialog)
-				 code_file.removeAndSubmitComment(current_range);
-				 });
+		 var code_file = code_files[current_file_id];
+		 if(current_range && current_dialog) {
+		    var comment = code_file.getCommentFromID("c" + current_range.toString());
+		    code_file.comment_list.push(comment);
+		    comment.submit();
+		 }
+	});
 	
 	shortcut.add("shift+0", function(){
-				 var code_file = code_files[current_file_id];
-				 if(current_range && current_dialog)
-				 code_file.removeComment(current_range);
-				 });
+	   var code_file = code_files[current_file_id];
+		if(current_range && current_dialog) {
+		    var comment = code_file.getCommentFromID("c" + current_range.toString());
+		    code_file.removeCommentFromID("c" + current_range.toString());
+		    comment.remove();
+		}
+	});
 	
 	shortcut.add("ctrl+z", function(){
-				 var code_file = code_files[current_file_id];
-				 if(code_file.last_comment_range == null) return;
-				 code_file.editComment(code_file.last_comment_range, current_file_id);
-				 code_file.last_comment_range = null;
-				 });
+		 var code_file = code_files[current_file_id];
+		 if(code_file.last_comment == null) return;
+		 code_file.last_comment.edit();
+		 code_file.last_comment = null;
+	});
 	
 	
 	shortcut.add("ctrl+2", new SafeFunction ( function(){
-											 themeID++;
-											 var themeIndex = themeID % themes.length; 
-											 var newTheme = root_url +'/static/js/syntaxhighlighter/styles/' + themes[themeIndex];
-											 $('#syntaxStylesheet').attr('href', newTheme);
-											 } )
-				 );
+		 themeID++;
+		 var themeIndex = themeID % themes.length; 
+		 var newTheme = root_url +'/static/js/syntaxhighlighter/styles/' + themes[themeIndex];
+		 $('#syntaxStylesheet').attr('href', newTheme);
+		 })
+    );
 	
 	
 	shortcut.add("ctrl+1", new SafeFunction ( function(){
@@ -82,439 +88,17 @@ function addShortcuts(){
 				 );
 }
 
-
-function /* class */ Comment(range, code_file){
-	this.range = range;
-	this.code_file = code_file;
-	this.filename = code_file.filename;
-	
-	this.submit = function(){
-		this.code_file.last_comment_range = this.range;
-		var commentText = $("textarea").val();
-		removeDialog();
-		if(commentText.length == 0){
-			this.code_file.unhighlightRange(range);
-		}else{
-			$.ajax({
-				   type: 'POST',
-				   url: window.location.pathname, // post to current location url
-				   data: "action=create&text=" + commentText + "&rangeLower=" + this.range.lower + "&rangeHigher=" + this.range.higher + "&filename=" + this.filename,
-				   success: function(data) {
-				   //TODO
-				   },
-				   error: function(XMLHttpRequest, textStatus, errorThrown) {
-				   //TODO
-				   }
-				   });
-			
-			this.addCommentDiv(commentText, range);
-		}
-	}
-	
-	this.remove = function() {
-		
-		var elem = "#e"+range.toString();
-		$(elem).remove(); // remove the comment
-		code_file.unhighlightRange(range);
-		
-		//remove it from selected ranges
-		for (i = 0; i < code_file.selected_ranges.length; i++) {
-			var saved_range = code_file.selected_ranges[i];
-			if (saved_range.lower == range.lower && saved_range.higher == range.higher) {
-				code_file.selected_ranges.splice(i, 1);
-			}
-		}
-		
-		$('textArea').remove();
-		removeDialog();
-	}
-	
-	this.edit = function() {
-		if (current_dialog != null){
-            return;
-		}
-		
-		// remove the old comment     
-		$.ajax({
-			   type: 'POST',
-			   url: window.location.pathname, // post to current location url
-			   data: "action=delete&rangeLower=" + this.range.lower + "&rangeHigher=" + this.range.higher + "&filename=" + this.filename,
-			   success: function(data) {
-			   // TODO
-			   },
-			   error: function(XMLHttpRequest, textStatus, errorThrown) {
-			   // TODO
-			   }
-			   });
-		
-		var text = $('#ctext' + this.range.toString()).html();
-		current_dialog = $('<div></div>')
-		.html('<textarea>' + text +'</textarea>')
-		.dialog({
-				autoOpen: true,
-				title: 'Enter Comment',
-				width: 350,
-				height: 250,
-				focus: true,
-				open: function(event, ui) { $(".ui-dialog-titlebar-close").hide();},
-				closeOnEscape: false,
-				buttons: { "Submit": 
-				function() {
-				this.removeAndSubmit();
-				}, "Delete":
-				function() {
-				this.remove();
-				}
-				}, 
-				
-				});
-		$("textarea").focus();
-	}
-	
-	this.cancel = function(range){
-		this.code_file.unhighlightRange(this.range);
-		removeDialog();
-    }
-	
-	this.removeAndSubmit = function(){
-		this.remove();
-		this.submit();
-	}
-}
-
-
-function /* class */ CodeFile(filename, prefix) {
-	if(!shortcuts_added)
-		addShortcuts();
-	
-	this.comment_list = null; // TODO make this hold a list of Comment objects
-	this.filename = filename;
-	this.prefix = prefix;
-	this.fileID = this.prefix; // right now pass the filename, and file id
-	
-	this.selected_range_start;
-	this.selected_range_end;
-	this.selected_ranges = [];
-	this.last_comment_range = null;
-	
-	var self = this;
-
-	this.isLineSelected = function(line_no) {
-		for (var i = 0; i < this.selected_ranges.length; i++) {
-			var range = this.selected_ranges[i];
-			if (line_no >= range.lower && line_no <= range.higher) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	this.addHandlers = function() {
-		var line_no = 1;
-		do {
-			var line = this.getLine(line_no);
-			line.bind("mousedown", {code_file : this}, CodeFile.mousePressed);
-			line.bind("mouseenter", {code_file : this}, CodeFile.mouseEntered);
-			line_no++;
-		} while(line.size() != 0);
-	}
-	
-	this.getLine = function(line_no) {
-		var id = "#file" + this.fileID;
-		var theclass = '.number' + line_no;
-		return $(theclass, id);
-	}
-	
-	this.getLineNumber = function(line) {
-		var newline = $(line).attr("class");
-		var pattern = /line number(\d+) .*/;
-		var result = pattern.exec(newline)[1];
-		return parseInt(result);
-	}
-	
-	this.hiliteLineNo = function(line_no) {
-		var id = "#file" + this.fileID;
-		var theclass = '.number' + line_no;
-		$(theclass, id).addClass('highlighted');
-	}
-	
-	this.unhiliteLineNo = function(line_no) {
-		var id = "#file" + this.fileID;
-		var theclass = '.number' + line_no;
-		$(theclass, id).removeClass('highlighted');
-	}
-	
-	this.hiliteLine = function (line) {
-		var line_no = this.getLineNumber(line);
-		$(line).addClass("highlighted");
-	}
-	
-	this.unhiliteLine = function (line) {
-		$(line).removeClass("highlighted");
-	}
-	
-	this.submitComment = function(range){
-		this.last_comment_range = range;
-		var commentText = $("textarea").val();
-		$('textarea').remove();
-		removeDialog();
-		if(commentText.length == 0){
-			this.unhighlightRange(range);
-		}else{
-			
-			$.ajax({
-				   type: 'POST',
-				   url: window.location.pathname, // post to current location url
-				   data: "action=create&text=" + commentText + "&rangeLower=" + range.lower + "&rangeHigher=" + range.higher + "&filename=" + self.filename,
-				   success: function(data) {
-				   //TODO
-				   },
-				   error: function(XMLHttpRequest, textStatus, errorThrown) {
-				   //TODO
-				   }
-				   });
-			
-			this.addCommentDiv(commentText, range);
-		}
-	}
-	
-    this.cancelComment = function(range){
-		this.unhighlightRange(range);
-		removeDialog();
-    }
-	
-	this.removeAndSubmitComment = function(range){
-		var elem = "#e"+this.rangeToString(range);
-		if(elem) $("#e"+this.rangeToString(range)).remove(); // remove the comment
-		this.submitComment(range);
-	}
-	
-	this.addCommentDiv = function(text, range){
-		var range_text = this.rangeToString(range);
-		var comment_id = "c" + this.rangeToString(range);
-		var element_id = "e" + this.rangeToString(range);
-		var top_offset = 17.6 * (range.lower) + 200; //200 is codeposition offset in style.css , 17 is line height;
-		var style_position = "position:absolute; top:"+ top_offset +"px; right: 100px;";
-		
-		
-		$('#comments' + this.fileID).append("<div id='"+ element_id +"'><a href=\"javascript:edit("+ this.fileID + ",'" + comment_id + "')\"> <div id='" + comment_id
-											+"' style='" + style_position +"' class='commentbox'>"
-											//	+ " File: " + this.filename 
-											//	+ " Line: "+ range_text + ": "
-											+ "<span id='ctext" + this.rangeToString(range) + "'>" + text + "</span>" 
-											+ "</div></a></div>");
-	}
-	
-	this.addCommentBox = function(range) {
-		var id = "#file" + this.fileID;
-		var theclass = '.number' + range.lower;
-		var commentLocation = $(theclass, id);
-		current_range = range;
-		
-		var comment = this;
-		
-		current_dialog = $('<div></div>')
-		.html('<textarea></textarea>')
-		.dialog({
-				autoOpen: true,
-				title: 'Enter Comment',
-				width: 350,
-				height: 250,
-				focus: true,
-				open: function(event, ui) { $(".ui-dialog-titlebar-close").hide();},
-				closeOnEscape: false,
-				buttons: { "Submit":
-				function() {
-				comment.submitComment(range); //because 'this' now refers to the dialog                                                                                        
-				},
-				"Cancel":
-				function() {
-				comment.cancelComment(range);
-				},
-				},
-				});
-		
-		$("textarea").focus();
-	}
-	
-	
-	/* Turns a Range into a string. If the range is one line, like 15-15, it returns 15
-	 * otherwise, it will return a string lower-higher, like 5-8 */
-	this.rangeToString = function(range){
-		var range_text = range.lower + "-" + range.higher;
-		if(range.lower == range.higher) range_text = range.lower;
-		return range_text;
-	}
-	
-	/* Unhighlights the range passed in as a parameter */
-	this.unhighlightRange = function(range){
-		for (var i = range.lower; i <= range.higher; i++) {
-			this.unhiliteLineNo(i);
-		}	
-	}
-	
-	
-	this.removeComment = function(range) {
-		
-		var elem = "#e"+this.rangeToString(range);
-		$("#e"+this.rangeToString(range)).remove(); // remove the comment
-		
-		this.unhighlightRange(range);
-		
-		//remove it from selected ranges
-		for (i = 0; i < this.selected_ranges.length; i++) {
-			var saved_range = this.selected_ranges[i];
-			if (saved_range.lower == range.lower && saved_range.higher == range.higher) {
-				this.selected_ranges.splice(i, 1);
-			}
-		}
-		
-		$('textArea').remove();
-		removeDialog();
-	}
-	
-	this.editComment = function(range, fileID) {
-		if (current_dialog != null){
-            return;
-		}
-		
-		// remove the old comment     
-		$.ajax({
-			   type: 'POST',
-			   url: window.location.pathname, // post to current location url
-			   data: "action=delete&rangeLower=" + range.lower + "&rangeHigher=" + range.higher + "&filename=" + self.filename,
-			   success: function(data) {
-			   // TODO
-			   },
-			   error: function(XMLHttpRequest, textStatus, errorThrown) {
-			   // TODO
-			   }
-			   });
-		
-		var text = $('#ctext' + this.rangeToString(range)).html();
-		current_dialog = $('<div></div>')
-		.html('<textarea>' + text +'</textarea>')
-		.dialog({
-				autoOpen: true,
-				title: 'Enter Comment',
-				width: 350,
-				height: 250,
-				focus: true,
-				open: function(event, ui) { $(".ui-dialog-titlebar-close").hide();},
-				closeOnEscape: false,
-				buttons: { "Submit": 
-				function() {
-				comment = code_files[fileID];
-				comment.removeAndSubmitComment(range); 
-				}, "Delete":
-				function() {
-				comment = code_files[fileID];
-				comment.removeComment(range);
-				}
-				}, 
-				
-				});
-		$("textarea").focus();
-	}
-	
-	this.addHandlers();
-}
-
-
-CodeFile.mousePressed = function(event) {
-	code_file = event.data.code_file;
-	if (dragging_in_file != null && dragging_in_file != code_file) {
-		return false;
-	}
-	
-	var line_no = code_file.getLineNumber(this);
-	if (current_dialog != null || code_file.isLineSelected(line_no)) {
-		return false;
-	}
-	
-	dragging_in_file = code_file;
-	code_file.selected_range_start = code_file.selected_range_end = line_no;
-	code_file.hiliteLine(this);
-	event.data.code_file.dragging = true;
-	return false;
-}
-
-CodeFile.mouseEntered = function(event) {
-	code_file = event.data.code_file;
-	if (dragging_in_file != code_file) {
-		return;
-	}
-	
-	var line_no = code_file.getLineNumber(this);
-	
-	// Trim range so it doesn't overlap any already selected lines.
-	var increment = (line_no >= code_file.selected_range_start ? 1 : -1);
-	for (var i = code_file.selected_range_start; !(code_file.isLineSelected(i)); i += increment) {
-		new_line_no = i;
-		if (i == line_no) {
-			break;
-		}
-	}
-	line_no = new_line_no;
-	
-	old_range = new LineRange(code_file.selected_range_start, code_file.selected_range_end);
-	range = new LineRange(code_file.selected_range_start, line_no);
-	range.each(function(line_no) {
-			   code_file.hiliteLineNo(line_no);
-			   });
-	
-	for (var i = old_range.lower; i < range.lower; i++) {
-		code_file.unfLineNo(i);
-	}
-	for (var i = old_range.higher; i > range.higher; i--) {
-		code_file.unhiliteLineNo(i);
-	}
-	
-	code_file.selected_range_end = line_no;
-}
-
-function /* class */ LineRange(a, b) {
-	this.lower = Math.min(a, b);
-	
-	this.higher = Math.max(a, b);
-	
-	this.toString = function() {
-		return "(" + this.lower + "," + this.higher + ")";
-	}
-	
-	this.contains = function(num) {
-		if (num >= lower && num <= higher) {
-			return true;
-		}
-		
-		return false;
-	}
-	
-	this.subtract = function(other) {
-		return new LineRange(Math.max(this.lower, other.lower), Math.min(this.higher, other.higher));
-	}
-	
-	this.each = function(callback) {
-		for (var i = this.lower; i <= this.higher; i++) {
-			callback(i);
-		}
-	}
-	
-	this.toString = function(){
-		var range_text = this.lower + "-" + this.higher;
-		if(this.lower == this.higher) range_text = this.lower;
-		return range_text;
-	}
-}
-
 $(document).mouseup(function() {
-					if (dragging_in_file == null) {
-					return;
-					}
-					
-					var range = new LineRange(dragging_in_file.selected_range_start, dragging_in_file.selected_range_end);
-					dragging_in_file.addCommentBox(range);
-					dragging_in_file.selected_ranges.push(range);
-					dragging_in_file = null;
-					});
+   if (dragging_in_file == null) {
+	   return;
+	}
+	
+   var range = new LineRange(dragging_in_file.selected_range_start, dragging_in_file.selected_range_end);
+   var comment = new Comment("", range, dragging_in_file);
+   comment.get();
+   
+   dragging_in_file.comment_list.push(comment);
+   dragging_in_file.selected_ranges.push(range);
+   
+   dragging_in_file = null;
+});
