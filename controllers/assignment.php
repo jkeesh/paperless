1d255b3ab2ef5d450510852dcd9d8a8eb5609609
Jeremy Keeshin <jkeesh@gmail.com>
2011-01-22 23:41:54 -0800

    add check and message to assignment view for sl-class sanity check

--- controllers/assignment.php
+++ controllers/assignment.php
@@ -4,6 +4,10 @@
 	class AssignmentHandler extends ToroHandler {
 		
 		public function get($class, $sectionleader, $assignment) {
+			$status = Model::getRoleForClass($sectionleader, $class); //sanity check: make sure they are visiting an sl for this class
+			if($status < POSITION_SECTION_LEADER){
+				Header("Location: " . ROOT_URL);
+			}
 
 			$role = Permissions::requireRole(POSITION_SECTION_LEADER, $class);
 						
@@ -16,9 +20,14 @@
 						
 			$dirname = SUBMISSIONS_PREFIX . "/" . $class . "/" . SUBMISSIONS_DIR . "/" . $sectionleader . "/";
 			$students = $this->getDirEntries($dirname . $assignment);
+			
+
 			sort($students);
 			
-			$this->smarty->assign("students", $students);
+			if(count($students[0]) > 0)
+				$this->smarty->assign("students", $students);
+			else
+				$this->smarty->assign("nothing", 1);
 			$this->smarty->assign("assignment", $assignment);
 			$this->smarty->assign("class", $class);
 			
