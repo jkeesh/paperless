611706c57775b84e4195df4b761200cfaf225fc6
Jeremy Keeshin <jkeesh@gmail.com>
2011-05-08 23:25:33 -0700

    small comment update to symlinks script

--- make-symlinks.py
+++ make-symlinks.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-#Adds symbolic links to assignments to section leaders' folders.  
-#Used when we need to make a submissions2 folder, but submissions needs 
-#to have every assignment in it for the sake of the paperless IG system.
-#
-#
+# Adds symbolic links to assignments to section leaders' folders.  
+# Used when we need to make a submissions2 folder, but submissions needs 
+# to have every assignment in it for the sake of the paperless IG system.
 #
 # How to run:
 # /afs/ir/class/cs198/cgi-bin/paperless/make-symlinks.py submissions submissions2 nameSurfer
-# This preserves the capability to find the assignment by only appending a number.
 #
 import os, sys
 
