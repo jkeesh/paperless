4b5188d9dbe459d8c3d115bc6b1bad21112e37ef
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-08 13:23:47 -0800

    add www to start of url

--- config_web.php
+++ config_web.php
@@ -21,7 +21,7 @@
 	
 	define('CLASS_CONFIG_DIR', '/afs/ir/class/cs198/cgi-bin/paperless/class_configs');
 	
-	define('ROOT_URL', 'https://stanford.edu/class/cs198/cgi-bin/paperless/');
+	define('ROOT_URL', 'https://www.stanford.edu/class/cs198/cgi-bin/paperless/');
 	
 	define('ASSIGNMENT_COMMENT_TABLE', "AssignmentComments");
 	define('ASSIGNMENT_FILE_TABLE', 'AssignmentFiles');
