f4e15f925095763d01f0745b610985010e795f3e
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-20 02:36:08 -0600

    add people soft code to quarter

--- models/Quarter.php
+++ models/Quarter.php
@@ -9,7 +9,8 @@ class Quarter extends Model {
 	public $quarter;
 	
 	public static $quarter_strings = array("Winter", "Spring", "Summer", "Fall");
-	
+	public static $people_soft_quarters = array('Fall' => 2, 'Winter' => 4, 'Spring' => 6, 'Summer' => 8);
+
 	private function quarter_string(){
 		return Quarter::$quarter_strings[$this->quarter - 1];
 	}
@@ -34,6 +35,26 @@ class Quarter extends Model {
 	
 	public function __toString(){
 		return $this->quarter_string() . ", " . $this->year;
+	}
+	
+	
+	/* 
+	 * The People Soft Quarter code is a very stupid code that tells us where the
+	 * afs mount point is.
+	 * The format is CYYQ, where 
+	 * C	= century code = 1
+	 * YY 	= year code = the last two digits of calendar year when academic year ends
+	 * Q	= quarter code , where 2 = Fall, 4 = Winter, 6 = Spring, 8 = Summer
+	 */
+	public function get_people_soft_code(){
+		if($this->quarter == 4){			
+			$people_soft_year = $this->year + 1;
+		}else{
+			$people_soft_year = $this->year;
+		}
+		$people_soft_year = substr($people_soft_year, 2, 2);
+		$people_soft_quarter = Quarter::$people_soft_quarters[$this->quarter_string()];
+		return '1'. $people_soft_year . $people_soft_quarter;
 	}
 }
 ?>
