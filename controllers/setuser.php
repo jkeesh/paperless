da28ce924893845d94f82c3fe9f98e322ece01d1
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-22 03:43:00 -0600

    comment set user

--- controllers/setuser.php
+++ controllers/setuser.php
@@ -2,6 +2,11 @@
 	
 	require_once('utils.php');
 	
+	/*
+	 * For debugging purposes, enable testing of different user's perspectives.
+	 * This is only setup for testing, and cannot be done by a random user
+	 * on the live site.
+	 */
 	class SetUser extends ToroHandler {
 				
 		public function get($user) {
