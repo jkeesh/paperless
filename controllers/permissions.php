140a68e3d9a899a9018e7654bf4609d66a2eab9d
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-22 03:40:51 -0600

    recoment permissions, and start to comment for verify method

--- controllers/permissions.php
+++ controllers/permissions.php
@@ -25,6 +25,8 @@ class Permissions {
 	 * and the actual role of the user in this class, redirect the user
 	 * to the home page if they do not have the required permissions
 	 *
+	 * $required	int, representing required permissions
+	 * $actual		int, representing the permissions this user has
 	 * @author	Jeremy Keeshin	December 22, 2011
 	 */
 	public static function gate($required, $actual) {
@@ -33,6 +35,17 @@ class Permissions {
 		}
 	}
 	
+	/*
+	 * Verify that $user has $role in $course. This is used as sanity
+	 * check in certain situations to make sure the information is 
+	 * consistent.
+	 * 
+	 * $role	int, representing the required role
+	 * $user	string, the sunet id of the user we are checking
+	 * $course	Course, the Course object that we are checking against
+	 *
+	 * @author	Jeremy Keeshin	December 22, 2011
+	 */
 	public static function verify($role, $user, $course){
 		
 	}
