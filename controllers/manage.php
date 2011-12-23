7e6e11d683e945543fccd4580a00a178d629149a
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-23 17:29:37 -0600

    refactor manage page

--- controllers/manage.php
+++ controllers/manage.php
@@ -7,13 +7,14 @@
 	 */
 	class ManageHandler extends ToroHandler {
 		
-		public function post($qid, $class){
+		private function manage($qid, $class, $handle_post=false){
 			$this->basic_setup(func_get_args());
 			Permissions::gate(POSITION_TEACHING_ASSISTANT, $this->role);		
-			
 			$quarter = Quarter::current();
 			
-			if($quarter->id == $qid){
+			if($quarter->id != $qid){
+				$this->smarty->assign("old_quarter", true);
+			}else if($handle_post){
 				if($_POST['action'] == "Update"){
 					PaperlessAssignment::update($_POST['id'], $class, $_POST['directory'], $_POST['name'], $_POST['duedate']);
 				}else if($_POST['action'] == "Delete"){
@@ -22,31 +23,19 @@
 					$assn = PaperlessAssignment::create($class, $_POST['directory'], $_POST['name'], $_POST['duedate']);
 					$assn->save();
 				}
-			}else{
-				$this->smarty->assign("old_quarter", true);
-			}
-			
-			$assns = PaperlessAssignment::loadForClass($class);
-			$this->smarty->assign("assignments", $assns);
-			$this->smarty->assign("class", $class);
-			$this->smarty->display('manage.html');	
-		}
-		
-		public function get($qid, $class) {
-			$this->basic_setup(func_get_args());
-			Permissions::gate(POSITION_TEACHING_ASSISTANT, $this->role);		
-
-			$quarter = Quarter::current();
-			
-			if($quarter->id != $qid){
-				$this->smarty->assign("old_quarter", true);
 			}
 
 			$assns = PaperlessAssignment::load_for_course($this->course);
-			// $assns = PaperlessAssignment::loadForClass($class);
 			$this->smarty->assign("assignments", $assns);
-			//$this->smarty->assign("class", $class);
 			$this->smarty->display('manage.html');
+		}
+		
+		public function post($qid, $class){
+			$this->manage($qid, $class, true);	
+		}
+		
+		public function get($qid, $class) {
+			$this->manage($qid, $class);
 		}
 	}
 	?>
