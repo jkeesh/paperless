306c6469442f89a1e97bb821557a691b1ea6098e
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-24 01:52:14 -0600

    remove config file and assns old csv way code

--- controllers/utils.php
+++ controllers/utils.php
@@ -33,31 +33,6 @@ class Utilities {
 	
 }
 
-
-
-/*
- * This reads of the assignment configuration information for a class
- * as specified by a csv file in the class configuration directory.
- */
-function getAssnsForClass($class) {
-	$assignments_file = getConfigFileForClass($class);
-	$assn_data = fopen($assignments_file, "r");
-
-	$arr = array();
-
-	fgetcsv($assn_data); //read off first three lines, not assignment data
-	fgetcsv($assn_data);
-
-	while (! feof($assn_data)) {
-		$info = fgetcsv($assn_data);
-		if ($info == NULL) {
-			continue;
-		}
-		$arr[$info[0]] = array("Name" => $info[1], "DueDate" => date_create($info[2]));
-	}
-	return $arr;
-}
-
 function getConfigFileForClass($class){
 	return CLASS_CONFIG_DIR . "/" . $class . ".csv";
 }
