c3fbfe60f90d6cdcfb59c27c1a865277588cabd6
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-24 01:23:14 -0600

    remove old require role method

--- controllers/permissions.php
+++ controllers/permissions.php
@@ -1,25 +1,6 @@
 <?php
 
-class Permissions {
-
-	public static function requireRole($role, $class) {
-		$curRole = Model::getRoleForClass(USERNAME, $class);
-
-		// Special case for cs198 coordinator access to cs106.
-		if($class == "cs106a" || $class == "cs106b" || $class == "cs106x"){
-			$cs198Role = Model::getRoleForClass(USERNAME, "cs198");
-			// If they are the coordinator, give them general SL access to the entire class.
-			if($cs198Role == POSITION_COORDINATOR && $role < POSITION_TEACHING_ASSISTANT){
-				return POSITION_SECTION_LEADER;
-			}
-		}
-
-		if($curRole < $role) {
-			Header("Location: " . ROOT_URL);
-		}
-		return $curRole;
-	}
-	
+class Permissions {	
 	/*
 	 * The main function of the permissions class. Given a required role,
 	 * and the actual role of the user in this class, redirect the user
