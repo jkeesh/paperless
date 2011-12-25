2662de144569321e5397be99e0ccd8af3d4c18f9
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-24 19:39:20 -0600

    start to work on settings page

--- controllers/settings.php
+++ controllers/settings.php
@@ -1,15 +1,20 @@
 <?php
 
-	class SettingsHandler extends ToroHandler {		
+require_once(dirname(dirname(__FILE__)) . "/models/Settings.php");
+
+
+	class SettingsHandler extends ToroHandler {				
 		public function post(){
 
 		}
 		
 		public function get() {
 			$this->basic_setup(func_get_args());
-			$this->smarty->assign("message", "Hi");	
 			
-			$this->smarty->display("message.html");			
+			$settings = Settings::get_for_user($this->user);
+			print_r($settings);
+			
+			$this->smarty->display("settings.html");			
 		}
 	}
 
