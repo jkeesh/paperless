460a11f97d05233a24f4bce20a78c2fd8cf22d27
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-24 22:38:06 -0600

    saving settings works basic on theme

--- controllers/settings.php
+++ controllers/settings.php
@@ -5,15 +5,33 @@ require_once(dirname(dirname(__FILE__)) . "/models/Settings.php");
 
 	class SettingsHandler extends ToroHandler {				
 		public function post(){
+			$this->basic_setup(func_get_args());
+			$settings = Settings::get_for_user($this->user);
 
+			foreach($_POST as $key => $val){
+				$settings->set_value($key, $val);
+			}
+			$settings->save();
+
+			$this->smarty->assign("settings", $settings);
+			$this->smarty->assign("settings_saved", true);
+			
+			$theme_options = Settings::get_options('theme');
+			$this->smarty->assign("theme_options", $theme_options);
+			$this->smarty->display("settings.html");			
 		}
 		
 		public function get() {
 			$this->basic_setup(func_get_args());
 			
 			$settings = Settings::get_for_user($this->user);
-			print_r($settings);
 			
+			$theme_options = Settings::get_options('theme');
+			
+			$this->smarty->assign("settings_saved", false);
+			
+			$this->smarty->assign("settings", $settings);
+			$this->smarty->assign("theme_options", $theme_options);
 			$this->smarty->display("settings.html");			
 		}
 	}
