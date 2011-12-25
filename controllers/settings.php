8e7a69d6b56d75cdf7b45a8bee45edc353ea5635
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-24 22:47:53 -0600

    update settings handler

--- controllers/settings.php
+++ controllers/settings.php
@@ -3,36 +3,34 @@
 require_once(dirname(dirname(__FILE__)) . "/models/Settings.php");
 
 
-	class SettingsHandler extends ToroHandler {				
-		public function post(){
+	class SettingsHandler extends ToroHandler {	
+		private function settings($handle_post = false){
 			$this->basic_setup(func_get_args());
 			$settings = Settings::get_for_user($this->user);
 
-			foreach($_POST as $key => $val){
-				$settings->set_value($key, $val);
+			if($handle_post){
+				foreach($_POST as $key => $val){
+					$settings->set_value($key, $val);
+				}
+				$settings->save();
+				$settings_saved = true;
+			}else{
+				$settings_saved = false;
 			}
-			$settings->save();
 
 			$this->smarty->assign("settings", $settings);
-			$this->smarty->assign("settings_saved", true);
-			
+			$this->smarty->assign("settings_saved", $settings_saved);			
 			$theme_options = Settings::get_options('theme');
 			$this->smarty->assign("theme_options", $theme_options);
-			$this->smarty->display("settings.html");			
+			$this->smarty->display("settings.html");				
+		}
+					
+		public function post(){
+			$this->settings(true);
 		}
 		
 		public function get() {
-			$this->basic_setup(func_get_args());
-			
-			$settings = Settings::get_for_user($this->user);
-			
-			$theme_options = Settings::get_options('theme');
-			
-			$this->smarty->assign("settings_saved", false);
-			
-			$this->smarty->assign("settings", $settings);
-			$this->smarty->assign("theme_options", $theme_options);
-			$this->smarty->display("settings.html");			
+			$this->settings();
 		}
 	}
 
