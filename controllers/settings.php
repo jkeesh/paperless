a45b86780555b646333a8c7c0d3d897d562f48d6
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-25 01:42:33 -0600

    modify redundant call to settings

--- controllers/settings.php
+++ controllers/settings.php
@@ -6,19 +6,18 @@ require_once(dirname(dirname(__FILE__)) . "/models/Settings.php");
 	class SettingsHandler extends ToroHandler {	
 		private function settings($handle_post = false){
 			$this->basic_setup(func_get_args());
-			$settings = Settings::get_for_user($this->user);
 
 			if($handle_post){
 				foreach($_POST as $key => $val){
-					$settings->set_value($key, $val);
+					$this->settings->set_value($key, $val);
 				}
-				$settings->save();
+				$this->settings->save();
 				$settings_saved = true;
 			}else{
 				$settings_saved = false;
 			}
 
-			$this->smarty->assign("settings", $settings);
+			$this->smarty->assign("settings", $this->settings);
 			$this->smarty->assign("settings_saved", $settings_saved);			
 			$theme_options = Settings::get_options('theme');
 			$this->smarty->assign("theme_options", $theme_options);
