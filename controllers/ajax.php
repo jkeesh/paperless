2e5c828b114674cbc8d6ee08f89d333e249a5bbc
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-25 01:07:00 -0600

    add tips and tip toggle

--- controllers/ajax.php
+++ controllers/ajax.php
@@ -4,6 +4,7 @@ class AjaxHandler extends ToroHandler {
 	public function post_xhr() {
 		$this->basic_setup(func_get_args());
 		
+		/* If the action was settings update the key=>value pair */
 		if($_POST['action'] == 'settings'){
 			$this->settings->set_value($_POST['key'], $_POST['val']);
 			$this->settings->save();
