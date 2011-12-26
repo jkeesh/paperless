e7222b7c7da7e0a2551dbe2d205fd4608d11bf41
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-25 23:57:46 -0600

    improve code file display to work based on id

--- static/js/CodeManager.js
+++ static/js/CodeManager.js
@@ -3,15 +3,25 @@ var CodeManager = {};
 /* Hide all of the code files */
 CodeManager.hide_all_files = function(){
     $('.code_container').hide();
+    $('.selectedFile').removeClass('selectedFile');
 }
 
 /* 
  * Display an individual file with the given name
  * @param   file    {string}    the name of the file
  */
-CodeManager.display_file = function(file){
+CodeManager.display_file = function(id){
     CodeManager.hide_all_files();
-    $('.code_container[data-name="'+file+'"]').show();
+    $('.code_container[data-id="'+id+'"]').show();
+    $('.filelink[data-id="'+id+'"]').addClass('selectedFile');
+}
+
+/*
+ * Helper to get a property value from a data-* attribute.
+ * @param   prop {string}       the name of the property
+ */
+CodeManager.get_property = function(elem, prop){
+    return $(elem).attr(prop);
 }
 
 /* 
@@ -19,17 +29,30 @@ CodeManager.display_file = function(file){
  * @param   elem    {elem}      the DOM element
  */
 CodeManager.get_name = function(elem){
-    return $(elem).attr('data-name');
+    return CodeManager.get_property(elem, 'data-name');
 }
 
+CodeManager.get_id = function(elem){
+    return CodeManager.get_property(elem, 'data-id');
+}
+
+
+
+/*
+ * Setup the functionality when a user clicks the name of the 
+ * file, then we should show that file. We get all of the file links
+ * and get their name, and each time they are clicked, display that
+ * file and hide the others.
+ */
 CodeManager.setup_file_selection = function(){
     $('.filelink a').each(function(idx, elem){
-        var name = CodeManager.get_name(elem);
+        var id = CodeManager.get_id(elem);
         $(this).click(function(e){
             e.preventDefault();
-            CodeManager.display_file(name);
+            CodeManager.display_file(id);
         });
     });
+    CodeManager.display_file(0); // Display the first file.
 }
 
 
