168fd77f6d66f009a1f9c7928470015ac9751969
Jeremy Keeshin <jkeesh@gmail.com>
2011-12-27 23:48:50 -0600

    get sl on public page in new way

--- public/sl.php
+++ public/sl.php
@@ -1,14 +1,23 @@
 <?php
+require_once("../models/SectionLeader.php");
+require_once("../models/Student.php");
+require_once("../models/Course.php");
+require_once("../models/PaperlessAssignment.php");
+require_once("../models/Quarter.php");
 
-//echo $sunetid;
-
-require_once("../models/Model.php");
-
-
+$quarter = Quarter::current();
+$class = "cs106a";
+$course = Course::from_name_and_quarter_id($class, $quarter->id);
 
 $sunetid = $_GET['student'];
-$class= "cs106a";
-$sl = Model::getSectionLeaderForStudent($sunetid, $class);
 
-echo $sl;
+$the_student = new Student;
+$the_student->from_sunetid_and_course($sunetid, $course);
+$sl = $the_student->get_section_leader();
+
+echo $sl->sunetid;
+// $class= "cs106a";
+// $sl = Model::getSectionLeaderForStudent($sunetid, $class);
+
+//echo $sl;
 ?>
