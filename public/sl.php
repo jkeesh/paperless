ecbced213be922e43eccb6f8f6d2637fd1bd2676
Jeremy Keeshin <jkeesh@gmail.com>
2012-01-12 17:42:29 -0800

    remove old way

--- public/sl.php
+++ public/sl.php
@@ -8,7 +8,6 @@ require_once("../models/Quarter.php");
 $quarter = Quarter::current();
 $class = "cs106a";
 $course = Course::from_name_and_quarter_id($class, $quarter->id);
-
 $sunetid = $_GET['student'];
 
 $the_student = new Student;
@@ -16,8 +15,4 @@ $the_student->from_sunetid_and_course($sunetid, $course);
 $sl = $the_student->get_section_leader();
 
 echo $sl->sunetid;
-// $class= "cs106a";
-// $sl = Model::getSectionLeaderForStudent($sunetid, $class);
-
-//echo $sl;
 ?>
