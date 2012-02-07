87527015071a3f32c11d1a2606df3e4a58810a26
Jeremy Keeshin <jkeesh@gmail.com>
2012-02-07 00:07:03 -0800

    Update section leader model to handle the case where people have multiple sections

--- models/SectionLeader.php
+++ models/SectionLeader.php
@@ -4,7 +4,8 @@ require_once(dirname(dirname(__FILE__)) . "/models/Student.php");
 
 class SectionLeader extends User {
 	
-	public $section_id;
+	// An array of usually one, but possibly more section ids
+	private $section_id;
 	
 	/*
 	 * Return an object that represnts the unknown section leader
@@ -13,15 +14,16 @@ class SectionLeader extends User {
 		$this->course = $course;
 		$this->sunetid = "unknown";
 	}
-
+	
 	/*
-	 * Static factory constructor to make a SectionLeader from a sunetid
-	 * and Course object.
+	 * Load a section leader. A section leader may have multiple sections, so we have
+	 * an array of section_ids.
+	 *
+	 * @author Jeremy Keeshin	February 7, 2012
 	 */
-	public function from_sunetid_and_course($sunetid, $course){
-		$this->from_sunetid($sunetid);
+	private function load_section_leader($course){
 		$this->course = $course;
-		
+		$this->section_id = array();
 		$db = Database::getConnection();
 		$query = "SELECT ID FROM Sections WHERE SectionLeader = :uid AND Quarter = :qid AND Class = :class_id";		
 		try {
@@ -29,46 +31,68 @@ class SectionLeader extends User {
 			$sth->execute(array(":uid" => $this->id, 
 								":qid" => $this->course->quarter->id, 
 								":class_id" => $this->course->id));
-			if($row = $sth->fetch()) {
-				$this->section_id = $row['ID'];				
-			}
+
+			$rows = $sth->fetchAll();			
+			foreach($rows as $row){
+				$this->section_id []= $row['ID'];
+			}			
 		} catch(PDOException $e) {
 			echo $e->getMessage(); // TODO log this error instead of echoing
 		}
+	}
+
+	/*
+	 * Static factory constructor to make a SectionLeader from a sunetid
+	 * and Course object.
+	 */
+	public function from_sunetid_and_course($sunetid, $course){
+		$this->from_sunetid($sunetid);
+		$this->load_section_leader($course);
 	}
 	
 	public function from_id_and_course($id, $course){
 		$this->from_id($id);
-		$this->course = $course;		
-		$db = Database::getConnection();
-		$query = "SELECT ID FROM Sections WHERE SectionLeader = :uid AND Quarter = :qid AND Class = :class_id";		
-		try {
-			$sth = $db->prepare($query);
-			$sth->execute(array(":uid" => $id, 
-								":qid" => $course->quarter->id, 
-								":class_id" => $course->id));
-			if($row = $sth->fetch()) {
-				$this->section_id = $row['ID'];
-			}
-		} catch(PDOException $e) {
-			echo $e->getMessage(); // TODO log this error instead of echoing
-		}
+		$this->load_section_leader($course);
 	}
 	
+	/*
+	 * Get the students for this SL. Since they may have multiple sections, we build up 
+	 * an array to include in the query
+	 *
+	 * @author	Jeremy Keeshin	February 7, 2012
+	 */
 	public function get_students(){
 		$db = Database::getConnection();
+
+		// Since a section leader may have more than one section in the same class (weird, right?)
+		// We need to construct an array and custom string query that may have multiple sections.
+		//
+		// The format is
+		//     :0 => <SECTION_ID_0>
+		//	   :1 => <SECTION_ID_1> ..
+		//
+		$sql_arr = array();
+		$counter = 0;
+		foreach($this->section_id as $id){
+			$sql_arr[':'.$counter] = $id;
+			$counter++;
+		}
 		
-		// if($sl_sunetid == "unknown"){
-		// 	return Model::getAllStudentsForClass($class);
-		// }
 		
+		// Then the query string becomes
+		// ... WHERE Section IN ( :0, :1 ...)
+		$inQuery = implode(',', array_keys($sql_arr));
+
 		$query = "SELECT ID, DisplayName, SUNetID, FirstName, LastName FROM People WHERE ID IN 
-					(SELECT Person FROM SectionAssignments WHERE Section = :section_id)";
-					
+					(SELECT Person FROM SectionAssignments WHERE Section IN 
+						(". $inQuery . ")
+					)";
+
 		$students = array();
 		try {
 			$sth = $db->prepare($query);
-			$sth->execute(array(":section_id" => $this->section_id));
+
+			$sth->execute($sql_arr);
 			while($row = $sth->fetch()){
 				$student = Student::from_row($row);
 				$student->set_course($this->course);
@@ -77,7 +101,6 @@ class SectionLeader extends User {
 		} catch(PDOException $e) {
 			echo $e->getMessage(); // TODO log this error instead of echoing
 		}
-		
 		return $students;
 	}	
 	
